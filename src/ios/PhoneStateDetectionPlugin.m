013a4ce68ba08ac4735bed5071ecb063d7f09ccb
Vahn84 <fabiocin84@gmail.com>
2016-05-10 13:33:39 +0200

    Version 1.0

--- src/ios/PhoneStateDetectionPlugin.m
+++ src/ios/PhoneStateDetectionPlugin.m
@@ -14,9 +14,15 @@
 
 @implementation PhoneStateDetectionPlugin : CDVPlugin
 
-    - (void) listen2PhoneSate:(CDVInvokedUrlCommand *) command {
-        CTCallCenter *callCenter = [[CTCallCenter alloc] init];
-        callCenter.callEventHandler=^(CTCall* call)
+    - (void) listenPhoneState:(CDVInvokedUrlCommand *) command {
+        self.callCenter = [[CTCallCenter alloc] init];
+        
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(callReceived:) name:CTCallStateIncoming object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(callEnded:) name:CTCallStateDisconnected object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(callConnected:) name:CTCallStateConnected object:nil];
+        self.isInPhoneCall = false;
+        self.isPhoneRinging = false;
+        self.callCenter.callEventHandler=^(CTCall* call)
         {
             
             if(call.callState == CTCallStateDialing)
@@ -28,21 +34,38 @@
             {
                 //The call state, before connection is established, when a call is incoming but not yet answered by the user.
                 NSLog(@"Call is Coming");
+                self.isPhoneRinging = true;
             }
             
             if(call.callState == CTCallStateConnected)
             {
                 //The call state when the call is fully established for all parties involved.
                 NSLog(@"Call Connected");
+                self.isInPhoneCall = true;
             }   
             
             if(call.callState == CTCallStateDisconnected)
             {
                 //The call state Ended.
                 NSLog(@"Call Ended");
+                NSMutableDictionary *jsonObj = [ [NSMutableDictionary alloc]
+                                                initWithObjectsAndKeys :
+                                                [NSNumber numberWithBool:_isPhoneRinging], @"isPhoneRinging",
+                                                [NSNumber numberWithBool:_isInPhoneCall], @"isInPhoneCall",
+                                                nil
+                                               ];
+                CDVPluginResult *pluginResult = [ CDVPluginResult
+                                                 resultWithStatus    : CDVCommandStatus_OK
+                                                 messageAsDictionary : jsonObj
+                                                 ];
+                [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
             }
             
         };
+    }
+
+    - (void) stopListenPhoneState:(CDVInvokedUrlCommand *) command {
+        [[NSNotificationCenter defaultCenter] removeObserver:self];
     }
 
 @end
